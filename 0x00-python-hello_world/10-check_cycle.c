8109a3e84caff662862b6f34a3201a9fef136fb9
Miguel Palacios <oswaldo9605@gmail.com>
2020-04-27 20:33:27 -0500

    Feat: mandatory linked list

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * check_cycle - check if a linked list is cycle or no
+ * @list: linked list
+ * Return: O or 1
+*/
+int check_cycle(listint_t *list)
+{
+	listint_t *current = list;
+
+	while (current != NULL)
+	{
+		current = current->next;
+		if (current == list)
+			return (1);	
+	}
+	return (0);
+}
