f8c309cf33ef0a3a6f682bd289cd5eac2a35df99
Miguel Palacios <oswaldo9605@gmail.com>
2020-04-29 14:07:17 -0500

    Feat: tack 13 insert

---
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -0,0 +1,57 @@
+#include "lists.h"
+/**
+ * create_node - creates a new node
+ * @n: int
+ * Return: address memory node
+ */
+listint_t *create_node(const int n)
+{
+	listint_t *n_node;
+
+	n_node = malloc(sizeof(listint_t));
+	if (n_node == NULL)
+		return (NULL);
+	n_node->n =  n;
+	n_node->next = NULL;
+	return (n_node);
+}
+
+/**
+ * insert_node - thisinserts a number into a
+ * sorted singly linked list.
+ * @head: dkdk
+ * @number: djdjd
+ * Return: djdjdjd
+*/
+listint_t *insert_node(listint_t **head, int number)
+{
+	listint_t *isrt_node, *tmp;
+
+	if (!head)
+		return (NULL);
+	isrt_node = create_node(number);
+	if (!isrt_node)
+		return (NULL);
+	if (isrt_node->n < (*head)->n)
+	{
+		isrt_node->next = *head;
+		*head = isrt_node;
+		return (isrt_node);
+	}
+	else
+	{
+		tmp = *head;
+		for (; tmp;)
+		{
+			if (isrt_node->n > tmp->n && isrt_node->n < tmp->next->n)
+			{
+				isrt_node->next = tmp->next;
+				tmp->next = isrt_node;
+				return (isrt_node);
+			}
+			tmp = tmp->next;
+		}
+
+	}
+	return (NULL);
+}
