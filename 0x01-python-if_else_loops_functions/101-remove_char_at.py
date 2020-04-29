ebd7af06ce00a1b9e7b6efb50ae29d264b481f58
Miguel Palacios <oswaldo9605@gmail.com>
2020-04-28 19:17:33 -0500

    fix: v3

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,6 +1,6 @@
 def remove_char_at(str, n):
     if n < len(str) and n >= 0:
         new_str = str[:n] + str[1 + n:]
-        return new_str
+        return (new_str)
     else:
-        return str
+        return (str)
