c16f29f6073fed6a8518ae3073762d09068ae3ca
Miguel Palacios <oswaldo9605@gmail.com>
2020-04-30 17:37:57 -0500

    Fix: output

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -9,4 +9,4 @@ if __name__ == "__main__":
         man = "arguments:"
     print("{} {}".format(len(argv) - 1, man))
     for i in range(1, len(argv)):
-        print("{} : {}".format(i, argv[i]))
+        print("{}: {}".format(i, argv[i]))
