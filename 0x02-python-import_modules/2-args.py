5b12dd6ea5eccc02bd69147b850cb71b1654a67a
Miguel Palacios <oswaldo9605@gmail.com>
2020-04-30 17:30:56 -0500

    Fix: major logic

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,15 +1,12 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     from sys import argv
-    count_arg = 0
     if len(argv) == 2:
-        count_arg += 1
-        print("{} argument:".format(count_arg))
-        print("{} : {}".format(count_arg, argv[1]))
+        man = "argument:"
     elif len(argv) < 2:
-        print("{} arguments.".format(count_arg))
+        man = "arguments."
     else:
-        count_arg = len(argv) - 1
-        print("{} arguments:".format(count_arg))
-        for i in range(1, count_arg + 1):
-            print("{} : {}".format(i, argv[i]))
+        man = "arguments:"
+    print("{} {}".format(len(argv) - 1, man))
+    for i in range(1, len(argv)):
+        print("{} : {}".format(i, argv[i]))
