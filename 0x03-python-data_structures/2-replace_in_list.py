4b83f752faee85222eb554eee93fe734dc364f5c
Miguel Palacios <oswaldo9605@gmail.com>
2020-05-05 13:04:51 -0500

    Fix: logic

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -2,7 +2,7 @@
 def replace_in_list(my_list, idx, element):
     if idx < 0:
         return my_list
-    if idx > len(my_list):
+    if idx > len(my_list) - 1:
         return my_list
     for i in range(len(my_list)):
         if i == idx:
--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -2,7 +2,7 @@
 def new_in_list(my_list, idx, element):
     if idx < 0:
         return my_list.copy()
-    if idx > len(my_list):
+    if idx > len(my_list) - 1:
         return my_list.copy()
     for i in range(len(my_list)):
         if idx == i:
