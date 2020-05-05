d07087421f0df87e34dcd6ef278de3fac3d540ec
Miguel Palacios <oswaldo9605@gmail.com>
2020-05-05 12:51:32 -0500

    Before of the checker
    On branch master
    Your branch is up to date with 'origin/master'.
    
    Changes to be committed:
    	new file:   0-print_list_integer.py
    	new file:   1-element_at.py
    	new file:   2-replace_in_list.py
    	new file:   3-print_reversed_list_integer.py
    	new file:   4-new_in_list.py
    	new file:   5-no_c.py

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    if idx < 0:
+        return my_list.copy()
+    if idx > len(my_list):
+        return my_list.copy()
+    for i in range(len(my_list)):
+        if idx == i:
+            new_list = my_list.copy()
+            new_list[i] = element
+            return new_list
