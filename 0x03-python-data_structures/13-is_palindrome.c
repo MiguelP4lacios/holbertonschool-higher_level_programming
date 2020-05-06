81ef886d0c84c8a07313e17d453f36196832f0da
Miguel Palacios <oswaldo9605@gmail.com>
2020-05-06 12:01:03 -0500

    Fix: logic

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -9,22 +9,17 @@ int is_palindrome(listint_t **head)
 {
 	listint_t * corrent;
 	size_t size, j;
-	int *palindro_array;
+	int palindro_array[2000];
 
 	if (*head == NULL)
 		return (1);
 	corrent = *head;
 	for (size = 0; corrent; size++)
-		corrent = corrent->next;
-	palindro_array = malloc(sizeof(int) * size);
-	if (!palindro_array)
-		return (1);
-	corrent = *head;
-	for (j = 0; j < size; j++)
 	{
-		palindro_array[j] = corrent->n;
+		palindro_array[size] = corrent->n;
 		corrent = corrent->next;
 	}
+	corrent = *head;
 	for (j = 0; j < size; j++, size--)
 	{
 		if (palindro_array[j] != palindro_array[size - 1])
