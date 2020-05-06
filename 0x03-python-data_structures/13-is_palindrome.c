50a4d0c4f96380b7b5a5d01107b9e2f0a8fbb860
Miguel Palacios <oswaldo9605@gmail.com>
2020-05-06 10:53:30 -0500

    Feat: linked list palindrome

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,34 @@
+#include "lists.h"
+/**
+ * 
+ * 
+ * 
+ * 
+*/
+int is_palindrome(listint_t **head)
+{
+	listint_t * corrent;
+	size_t size, j;
+	int *palindro_array;
+
+	if (*head == NULL)
+		return (1);
+	corrent = *head;
+	for (size = 0; corrent; size++)
+		corrent = corrent->next;
+	palindro_array = malloc(sizeof(int) * size);
+	if (!palindro_array)
+		return (1);
+	corrent = *head;
+	for (j = 0; j < size; j++)
+	{
+		palindro_array[j] = corrent->n;
+		corrent = corrent->next;
+	}
+	for (j = 0; j < size; j++, size--)
+	{
+		if (palindro_array[j] != palindro_array[size - 1])
+			return (0);
+	}
+	return (1);
+}
