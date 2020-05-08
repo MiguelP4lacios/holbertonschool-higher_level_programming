c7bd1d9099874c34d962fc40b90e91ecc37c8676
Miguel Palacios <oswaldo9605@gmail.com>
2020-05-07 20:02:21 -0500

    Fix: pep8

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,6 +2,6 @@
 def best_score(a_dictionary):
     if a_dictionary is None or a_dictionary == {}:
         return None
-    if max(a_dictionary, key = lambda k: a_dictionary[k]) is 0:
+    if max(a_dictionary, key=lambda k: a_dictionary[k]) is 0:
         return None
-    return max(a_dictionary, key = lambda k: a_dictionary[k])
+    return max(a_dictionary, key=lambda k: a_dictionary[k])
--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    symbols = {'I': 1, 'V':5, 'X':10, 'L':50, 'C':100, 'D':500, 'M':1000} 
+    symbols = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     num = 0
     if type(roman_string) is not str or roman_string is None:
         return 0
