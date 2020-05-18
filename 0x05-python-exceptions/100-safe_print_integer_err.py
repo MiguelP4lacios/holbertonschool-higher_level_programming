59b58f8e19f6d2fc6eeecf67c6420ca46f6c8e78
Miguel Palacios <oswaldo9605@gmail.com>
2020-05-18 17:35:06 -0500

    Fix: except

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -5,10 +5,8 @@ def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
         return True
-    except ValueError as men:
+    except (ValueError, TypeError) as men:
         strmen += str(men)
         strmen += "\n"
         sys.stderr.write(strmen)
-        return False
-    except TypeError:
         return False
