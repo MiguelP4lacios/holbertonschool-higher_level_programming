ff27c5068fb32250ab77d115a06aba4419a72a15
Miguel Palacios <oswaldo9605@gmail.com>
2020-05-23 17:59:05 -0500

    Fix: more assert

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -12,9 +12,37 @@ class testing_max_integer(unittest.TestCase):
     Arguments:
         unittest {[type]} -- [description]
     """
-    def test_1(self):
-        """test_1
+    def test_middle(self):
+        """test_middle
         """
         self.assertAlmostEquals(max_integer([1,2,5,4]), 5)
 
+    def test_end(self):
+        """test_middle
+        """
+        self.assertAlmostEquals(max_integer([1,2,4,5]), 5)
 
+    def test_beginning(self):
+        """test_middle
+        """
+        self.assertAlmostEquals(max_integer([5,2,4,2]), 5)
+
+    def test_only_negative_numbers(self):
+        """test_middle
+        """
+        self.assertAlmostEquals(max_integer([-1,-3,-100,-4]), -1)
+
+    def test_one_negative_number(self):
+        """test_middle
+        """
+        self.assertAlmostEquals(max_integer([-100]), -100)
+
+    def test_one_number(self):
+        """test_middle
+        """
+        self.assertAlmostEquals(max_integer([100]), 100)
+
+    def test_empty(self):
+        """test_middle
+        """
+        self.assertFalse(max_integer([]))
