f4d594de0e6ee0088e4065568804b47800b8d715
Miguel Palacios <oswaldo9605@gmail.com>
2020-06-02 19:34:10 +0000

    Feat: lookup, muy_list(MyList inheritance from list)

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Module 
+"""
+
+
+class MyList(list):
+        """Class
+
+        Arguments:
+            list {[type]} -- [description]
+        """
+
+        def print_sorted(self):
+                """Method
+                """
+                print(sorted(self))
