19c86e2b1958d090ba4aec8b37b96b6446079a51
Miguel Palacios <oswaldo9605@gmail.com>
2020-06-03 01:48:50 +0000

    Fix: test

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -2,29 +2,30 @@
 """[summary]
 
 Raises:
-        Exception: [description]
-        TypeError: [description]
-        ValueError: [description]
+    Exception: [description]
+    TypeError: [description]
+    ValueError: [description]
 """
 
+
 class BaseGeometry():
-        """Class
+    """Class
+    """
+
+    def area(self):
+        """Method 1
+        """
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """Method 2
+
+        Arguments:
+            name {[type]} -- [description]
+            value {[type]} -- [description]
         """
 
-        def area(self):
-                """Method 1
-                """
-                raise Exception("area() is not implemented")
-
-        def integer_validator(self, name, value):
-                """Method 2
-
-                Arguments:
-                    name {[type]} -- [description]
-                    value {[type]} -- [description]
-                """
-
-                if type(value) is not int:
-                        raise TypeError("{} must be an integer".format(name))
-                if value <= 0:
-                        raise ValueError("{} must be greater than 0".format(name))
+        if type(value) is not int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
