1269f3af012827585c27e4df1227a6d8a38184b0
Miguel Palacios <oswaldo9605@gmail.com>
2020-06-04 04:04:24 +0000

    Feat: pascal

---
+++ 0x0B-python-input_output/14-pascal_triangle.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""pascal triangle
+"""
+
+
+def pascal_triangle(n):
+    """Function
+    """
+    triangle = [
+            [1],
+            [1, 1]]
+    for r in range(1, n-1):
+        row = [1]
+        for c in range(0, len(triangle[r]) - 1):
+            row.extend([triangle[r][c] + triangle[r][c+1]])
+        row += [1]
+        triangle.append(row)
+    return triangle
