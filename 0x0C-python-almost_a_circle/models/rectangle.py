dd80c1d53e5dac51176e88395f601cc51c2af96a
Miguel Palacios <oswaldo9605@gmail.com>
2020-06-09 18:02:00 +0000

    Fix: docstring

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
+"""Module rectangle
+"""
+
 
 from models.base import Base
 
 
 class Rectangle(Base):
+        """Class rectangle"""
 
         def __init__(self, width, height, x=0, y=0, id=None):
 
