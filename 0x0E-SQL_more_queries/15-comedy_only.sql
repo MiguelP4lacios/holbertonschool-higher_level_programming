4561e24062a6e3405006b80e78c857b1df065ef4
Miguel Palacios <oswaldo9605@gmail.com>
2020-07-15 03:28:33 +0000

    [feature]: only comedy

---
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -0,0 +1,7 @@
+-- Only comedy
+
+SELECT title FROM tv_shows
+INNER JOIN tv_show_genres ON tv_shows.id=tv_show_genres.show_id
+INNER JOIN tv_genres ON tv_show_genres.genre_id=tv_genres.id
+WHERE name='Comedy'
+ORDER BY title ASC
