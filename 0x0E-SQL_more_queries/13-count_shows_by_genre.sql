de67457ec6db1fe7b459ca0a337f4e20bdbc23dd
Miguel Palacios <oswaldo9605@gmail.com>
2020-07-15 03:45:51 +0000

    [Fix]: Style sql and literal stirng

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,7 +1,7 @@
 -- script that lists all shows contained
 -- in hbtn_0d_tvshows without a genre linked.
-SELECT tv_genres.name AS genre, COUNT(tv_show_genres.genre_id) AS number_shows
-FROM tv_genres INNER JOIN tv_show_genres 
-ON tv_show_genres.genre_id = tv_genres.id 
-GROUP BY tv_genres.name
+SELECT tv_genres.name AS genre, COUNT(tv_show_genres.genre_id) AS 'number_shows'
+FROM tv_genres 
+INNER JOIN tv_show_genres ON tv_show_genres.genre_id = tv_genres.id 
+GROUP BY genre
 ORDER BY number_shows DESC;
