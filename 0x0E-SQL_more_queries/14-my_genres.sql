8b7517a99a6203cb7676d4c8549f626c862a9d16
Miguel Palacios <oswaldo9605@gmail.com>
2020-07-15 03:00:54 +0000

    Feature: My genres

---
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -0,0 +1,10 @@
+-- script that lists all shows contained
+-- in hbtn_0d_tvshows without a genre linked.
+
+
+SELECT  tv_genres.name
+FROM tv_genres
+INNER JOIN tv_show_genres ON tv_genres.id = tv_show_genres.show_id
+INNER JOIN tv_shows ON tv_shows.id = tv_show_genres.show_id
+WHERE tv_shows.title = 'Dexter'
+ORDER BY tv_genres.name;
