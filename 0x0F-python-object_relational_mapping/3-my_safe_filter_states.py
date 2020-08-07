bf3761b6715c92af34e8e08ffcd1f3b1f0fbbaf6
Miguel Palacios <oswaldo9605@gmail.com>
2020-08-07 23:22:09 +0000

    [feature]: safe

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""Module 2_my_filter_states
+"""
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+
+    username = argv[1]
+    password = argv[2]
+    database = argv[3]
+    searched = argv[4]
+
+    db = MySQLdb.connect(host="localhost", port=3306,
+                         user=username, passwd=password, db=database, charset="utf8")
+
+    query = db.cursor()
+
+    query.execute("SELECT * FROM states WHERE name = %(searched)s ORDER BY id ASC", {'searched':searched})
+    for row in query.fetchall():
+        print(row)
+    query.close()
+    db.close()
