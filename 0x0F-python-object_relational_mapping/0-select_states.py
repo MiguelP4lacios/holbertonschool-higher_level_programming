6e1bba6714418469dea31afba76f0f24c6056066
Miguel Palacios <oswaldo9605@gmail.com>
2020-08-07 20:48:55 +0000

    [feature]: select states with python3

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""Module 0_select_states
+"""
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+
+    username = argv[1]
+    password = argv[2]
+    database = argv[3]
+
+    db = MySQLdb.connect(host="localhost", port=3306,
+                         user=username, passwd=password, db=database, charset="utf8")
+
+    query = db.cursor()
+
+    query.execute("SELECT * FROM states ORDER BY id ASC")
+    for row in query.fetchall():
+        print(row)
+    query.close()
+    db.close()
