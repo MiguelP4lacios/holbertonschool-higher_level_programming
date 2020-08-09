6568912162b8654cd2da3851cbddb40d5b1c0429
Miguel Palacios <oswaldo9605@gmail.com>
2020-08-09 22:09:41 +0000

    [feature]: filter a

---
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""Module 7_model_state_fetch_all
+"""
+from sys import argv
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from re import search
+
+if __name__ == "__main__":
+    # Connection → mysql+mysqldb://<user>:<passwd>@<host>:<port>/<name_db>'
+    user = argv[1]
+    passwd = argv[2]
+    db = argv[3]
+
+    database_url = 'mysql+mysqldb://{}:{}@localhost:3360/{}'.format(
+        user, passwd, db)
+
+    engine = create_engine(database_url, pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    session_basedata = sessionmaker(bind=engine)
+    init_basedata = session_basedata()
+    query = init_basedata.query(State)
+
+    for state in query.order_by(State.id).all():
+        if search("(a)", state.name):
+            print('{}: {}'.format(state.id, state.name))
+
+    init_basedata.close()
