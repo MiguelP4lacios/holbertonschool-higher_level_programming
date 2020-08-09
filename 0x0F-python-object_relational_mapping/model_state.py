61f046d3463db2b77a58570e3d4d89dc2406cff5
Miguel Palacios <oswaldo9605@gmail.com>
2020-08-09 20:48:07 +0000

    [feature ]: state model

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""Model states
+"""
+
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class State(Base):
+    """State class
+
+    Args:
+        Base (class): basic class
+    """
+    __tablename__ = 'states'
+    id = Column(Integer,
+                primary_key=True,
+                autoincrement=True,
+                unique=True,
+                nullable=True)
+    name = Column(String(128),
+                  nullable=True)
