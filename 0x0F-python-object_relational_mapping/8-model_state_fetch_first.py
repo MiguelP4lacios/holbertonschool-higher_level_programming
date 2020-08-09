3bed4ab931307c6e0244f8858b6fa299c8b6bdd7
Miguel Palacios <oswaldo9605@gmail.com>
2020-08-09 21:37:36 +0000

    [feature]: first state

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+"""Module 7_model_state_fetch_all
+"""
+from sys import argv
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    # Connection → mysql+mysqldb://<user>:<passwd>@<host>:<port>/<name_db>'
+    user = argv[1]
+    passwd = argv[2]
+    db = argv[3]
+
+    database_url = 'mysql+mysqldb://{}:{}@localhost:3360/{}'.format(
+        user, passwd, db)
+
+    engine = create_engine(database_url, pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    session_basedata = sessionmaker(bind=engine)
+    init_basedata = session_basedata()
+    state = init_basedata.query(State).first()
+
+    if state:
+        print('{}: {}'.format(state.id, state.name))
+    else:
+        print("Nothing")
+
+    init_basedata.close()
