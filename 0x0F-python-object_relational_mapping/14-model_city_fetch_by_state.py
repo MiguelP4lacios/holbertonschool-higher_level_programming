37c534fd8083d6ad4e77caacf8785068c6ac9fc5
Miguel Palacios <oswaldo9605@gmail.com>
2020-08-10 02:01:54 +0000

    [Feature]: city

---
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""Module 9_model_state_fetch_a
+"""
+from sys import argv
+from model_state import Base as Base_state, State
+from model_city import City, Base as Base_city
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    # Connection → mysql+mysqldb://<user>:<passwd>@<host>:<port>/<name_db>'
+    user = argv[1]
+    passwd = argv[2]
+    db = argv[3]
+
+    database_url = 'mysql+mysqldb://{}:{}@localhost:3360/{}'.format(
+        user, passwd, db)
+
+    engine = create_engine(database_url, pool_pre_ping=True)
+    session_basedata = sessionmaker(bind=engine)
+    init_basedata = session_basedata()
+    query = init_basedata.query(State, City).filter(
+        City.state_id == State.id).order_by(City.id).all()
+
+    for record_s, record_c in query:
+        print("{}: ({}) {}".format(record_s.name, record_c.id, record_c.name))
+    init_basedata.close()
