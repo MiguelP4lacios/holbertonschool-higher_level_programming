5279cb2a7c7b950d820c510ed0f712fef19866cb
Miguel Palacios <oswaldo9605@gmail.com>
2020-08-18 21:49:47 +0000

    [fix]: comment

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Bash script that takes in a URL, sends a GET request to the URL, and displays the body of the response
+# Bash script that takes in a URL, sends a GET request to the URL
 status=$(curl -sI "$1" | head -n +1 | awk '{print $2}')
 if [ "$status" == "200" ]; then
         curl -L "$1"
