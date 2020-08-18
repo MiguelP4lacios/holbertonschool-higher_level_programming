5b63964179f8e53a3493e2e316dc2f382577ed16
Miguel Palacios <oswaldo9605@gmail.com>
2020-08-18 20:49:46 +0000

    [fix]: awk → cut

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -3,4 +3,4 @@
 # sends a request to that URL, 
 # and displays the size of the body of the response
 
-curl -sI $1 | grep Content-Length | awk '{print $2}'
+curl -sI $1 | grep Content-Length | cut -d ' ' -f2
