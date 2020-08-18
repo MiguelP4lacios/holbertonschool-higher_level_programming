8cf9d16dce19f72a8720dd5b260b382e9d5666a6
Miguel Palacios <oswaldo9605@gmail.com>
2020-08-18 21:56:15 +0000

    [fix]: 3 line

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 # Bash script that takes in a URL, sends a GET request to the URL
-status=$(curl -sI "$1" | head -n +1 | awk '{print $2}')
-if [ "$status" == "200" ]; then
-        curl -L "$1"
-fi
+curl -sL "$1"
