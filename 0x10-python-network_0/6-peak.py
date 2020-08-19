5871761bf4b581ed08b0d8f546e670b259ad47b9
Miguel Palacios <oswaldo9605@gmail.com>
2020-08-19 04:20:24 +0000

    [feature]: peak

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,10 @@
+#!/usr/bin/bash
+"""find peak
+"""
+def find_peak(list_of_integers):
+    """sort peak
+    """
+    if len(list_of_integers) == 0:
+        return None
+    list_of_integers.sort()
+    return list_of_integers[-1]
