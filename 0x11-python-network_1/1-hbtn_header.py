c8030ac0162de098ea0e4c3bceb512d3965fcc1a
Miguel Palacios <oswaldo9605@gmail.com>
2020-08-24 21:11:09 +0000

    [fix]: variable

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -7,8 +7,5 @@ from urllib import request
 from sys import argv
 
 
-URL = str(argv[1])
-
-
-with request.urlopen(URL) as response:
+with request.urlopen(argv[1]) as response:
     print(response.headers['X-Request-Id'])
