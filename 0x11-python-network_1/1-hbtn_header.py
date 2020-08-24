70ce2a2c887f8010ad1c7a5358003a8dd0b07517
Miguel Palacios <oswaldo9605@gmail.com>
2020-08-24 21:14:17 +0000

    [fix]: requeriment

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -3,9 +3,11 @@
 """
 
 
-from urllib import request
-from sys import argv
+if __name__ == "__main__":
+
+    from urllib import request
+    from sys import argv
 
 
-with request.urlopen(argv[1]) as response:
-    print(response.headers['X-Request-Id'])
+    with request.urlopen(argv[1]) as response:
+        print(response.headers.get('X-Request-Id'))
