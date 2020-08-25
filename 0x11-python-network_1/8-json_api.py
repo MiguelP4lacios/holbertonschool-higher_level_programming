aa049958eec9180e557d43311662f17b01ee7c2e
Miguel Palacios <oswaldo9605@gmail.com>
2020-08-25 01:30:14 +0000

    feat: api

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""json api
+"""
+
+if __name__ == "__main__":
+
+    import requests
+    from sys import argv
+
+    url = 'http://7ffe2036a3c6.9790342a.hbtn-cod.io:5000/search_user'
+
+    q = ""
+    if len(argv) == 2:
+        q = argv[1]
+
+    data = {'q': q}
+
+    try:
+        res = requests.post(url, data).json()
+
+        if res:
+            print('[{}] {}'.format(res.get('id'), res.get('name')))
+        else:
+            print("No result")
+    except ValueError:
+        print("Not a valid JSON")
