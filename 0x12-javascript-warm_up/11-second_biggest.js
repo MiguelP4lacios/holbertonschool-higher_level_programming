887bf514d5b87eaceff41b9dd78ce01caaa679df
Miguel Palacios <oswaldo9605@gmail.com>
2020-09-15 21:59:04 +0000

    [fix]: 1 and NaN

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,12 +1,17 @@
 #!/usr/bin/node
 
 const arg = process.argv;
-const listInt = [];
-arg.forEach((val, index) => {
-  if (index >= 2) {
-    listInt.push(parseInt(val, 10));
-  }
-});
 
-listInt.sort();
-console.log(listInt[listInt.length - 2]);
+if (!arg[2] || arg[2] === '1') {
+  console.log(0);
+} else {
+  const listInt = [];
+  arg.forEach((val, index) => {
+    if (index >= 2) {
+      listInt.push(parseInt(val, 10));
+    }
+  });
+
+  listInt.sort();
+  console.log(listInt[listInt.length - 2]);
+}
