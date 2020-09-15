c7201c912bdd8d4bdd4d515d9472d604e1e11266
Miguel Palacios <oswaldo9605@gmail.com>
2020-09-15 20:22:32 +0000

    [fix]: const

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
-let multi = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+const multi = ['C is fun', 'Python is cool', 'Javascript is amazing'];
 
 for (const language in multi) {
   console.log(`${multi[language]}`);
