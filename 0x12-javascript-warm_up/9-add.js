f7c81881a08092a36304845d910b9f06647ec986
Miguel Palacios <oswaldo9605@gmail.com>
2020-09-15 20:55:39 +0000

    [fix]: miss space prototype

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 
-function add (a, b) {
+function add(a, b) {
   console.log(a + b);
-};
+}
 
 const num1 = parseInt(process.argv[2], 10);
 const num2 = parseInt(process.argv[3], 10);
