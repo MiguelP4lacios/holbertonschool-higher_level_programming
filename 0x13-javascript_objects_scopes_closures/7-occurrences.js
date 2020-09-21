770ae44f8429ccdf8833275d344d169f344d1aa7
Miguel Palacios <oswaldo9605@gmail.com>
2020-09-21 20:30:38 +0000

    [Fueture]: Occurences

---
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+exports.nbOccurences = function (list, searchElement) {
+  let count = 0;
+  for (const key in list) {
+    if (list[key] === searchElement) {
+      count += 1;
+    }
+  }
+  return count;
+}
