3262008f6361a53f9cf060f0af7ceaf9980e6849
Miguel Palacios <oswaldo9605@gmail.com>
2020-09-22 18:26:47 +0000

    [Fix]: error menssage

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 
 fs.readFile(process.argv[2], 'utf-8', (err, data) => {
   if (err) {
-    console.log('Error:', err);
+    console.log(err);
   } else {
     console.log(data.toString());
   }
