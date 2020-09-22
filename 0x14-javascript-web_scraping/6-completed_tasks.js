52d53fc0bd66d0307981e7ee4904e6482b1c8626
Miguel Palacios <oswaldo9605@gmail.com>
2020-09-22 19:41:01 +0000

    [feature]: How many completed?

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,23 @@
+#!/usr/bin/node
+const request = require('request');
+
+const URL = process.argv[2];
+
+request(URL, (error, response, body) => {
+  if (error) {
+    console.error('error:', error); // Print the error if one occurred
+  } else {
+    const task = JSON.parse(body);
+    const infoTasks = {};
+    task.forEach(element => {
+      if (element.completed === true) {
+        if (infoTasks[element.userId] === undefined) {
+          infoTasks[element.userId] = 1;
+        } else {
+          infoTasks[element.userId]++; 
+        }
+      }
+    });
+    console.log(infoTasks);
+  }
+});
